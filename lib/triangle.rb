528dd0fbe47a7501fc1fcfbcb3f88645f83f84e1
Sarah Mukuti <cesarwrites24@gmail.com>
2022-09-06 14:37:06 +0300

    commit tree b3ba976410e0033d2987c5ba218cf2cfd7803180

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
-end
+    attr_accessor :length1, :length2, :length3
+  
+    def initialize(length1, length2, length3)
+      @length1 = length1
+      @length2 = length2
+      @length3 = length3
+    end
+    def kind
+     if (@length1 > 0 && @length2 > 0 && @length3 > 0) && (@length1 + @length2 > @length3 && @length1 + @length3 > @length2 && @length2 + @length3 > @length1)
+        if @length1 == @length2 && @length1 == @length3
+          :equilateral
+        elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+          :isosceles
+        else 
+          :scalene
+        end
+      else 
+        raise TriangleError
+      end
+    end
+  
+    class TriangleError < StandardError
+    end
+  
+  end
+  triangle1 = Triangle.new(1,1,1)
+  triangle2 = Triangle.new(1,2,2)
+  puts triangle1.kind
+  puts triangle2.kind
